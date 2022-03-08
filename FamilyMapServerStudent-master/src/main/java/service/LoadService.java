db62ee8b9a59df2f41ac7bc9f6e0e4ea79611329
jacobcwright <jacob.wright.ut@gmail.com>
2022-03-07 20:34:49 -0700

    implemented Load Service

--- FamilyMapServerStudent-master/src/main/java/service/LoadService.java
+++ FamilyMapServerStudent-master/src/main/java/service/LoadService.java
@@ -1,5 +1,9 @@
 package service;
 
+import dao.*;
+import model.Event;
+import model.Person;
+import model.User;
 import request.LoadRequest;
 import result.LoadResult;
 
@@ -13,8 +17,39 @@ public class LoadService {
      * @param l
      * @return
      */
-    public LoadResult load(LoadRequest l){
-        return null;
+    public LoadResult load(LoadRequest l) throws DataAccessException {
+        System.out.println("in LoadService");
+        Database db = new Database();
+
+        try{
+            db.openConnection();
+            // clear database
+            new UserDAO(db.getConnection()).clear();
+            new PersonDAO(db.getConnection()).clear();
+            new EventDAO(db.getConnection()).clear();
+
+            // load database with Users, Persons, and Events
+            for(User u : l.getUsers()){
+                new UserDAO(db.getConnection()).insert(u);
+            }
+            for(Person p : l.getPersons()){
+                new PersonDAO(db.getConnection()).insert(p);
+            }
+            for(Event e : l.getEvents()){
+                new EventDAO(db.getConnection()).insert(e);
+            }
+
+            db.closeConnection(true);
+
+            LoadResult result = new LoadResult(true, "Successfully added " + l.getUsers().length + " users, " +
+                    l.getPersons().length + " persons, and " + l.getEvents().length + " events to the database.");
+            return result;
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            db.closeConnection(false);
+            return new LoadResult(false, "Error: " + e.getMessage());
+        }
     }
 
 }
